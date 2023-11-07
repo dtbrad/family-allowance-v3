9387497b9d53d8c71762ace8ab3590746e20f9c4
Brad <daniel.brad@sap.com>
2023-11-07 15:09:21 -0500

    fix delete user operation

--- src/db/deleteUser.ts
+++ src/db/deleteUser.ts
@@ -3,12 +3,28 @@ import {dynamo} from './dynamo';
 const tableName = process.env.TABLE_NAME || '';
 
 export default async function deleteUser(userId: string) {
-    const lowerCaseUserId = userId.toLowerCase();
-
-    await dynamo
-        .delete({
+    const userQueryResponse = await dynamo
+        .query({
             TableName: tableName,
-            Key: {PK: lowerCaseUserId, SK: lowerCaseUserId}
+            KeyConditionExpression: 'PK = :pk',
+            ExpressionAttributeValues: {
+                ':pk': userId
+            }
         })
         .promise();
+
+    if (!userQueryResponse?.Items) {
+        return;
+    }
+
+    await Promise.all(
+        userQueryResponse.Items.map(async (item) => {
+            await dynamo
+                .delete({
+                    TableName: process.env.TABLE_NAME!,
+                    Key: {PK: item.PK, SK: item.SK}
+                })
+                .promise();
+        })
+    );
 }
